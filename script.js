3c31d9e717529cb6612170caf788474f4c6f7952
mklapshin <golub.97@mail.ru>
2021-06-07 12:21:03 +0300

    выполнил домашнее задание № 4

--- script.js
+++ script.js
@@ -1,45 +1,31 @@
-class GoodsItem {
-  constructor(title, price) {
-    this.title = title;
-    this.price = price;
-  }
-  render() {
-    return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p></div>`;
-  }
-}
+const str = "We are great, aren't we? 'My name is Mike'. 'Граждане, соблюдайте порядок!', - сказал он. Я шел по улице и говорил: 'Привет', потом я говорил: 'Ответ', а потом я встретил прохожего и он мне сказал: 'Иди к черту!'."
+console.log(str)
 
-class GoodsList {
-  constructor() {
-    this.goods = [];
-  }
+const newstr = str.replaceAll(" '", ' "')
+const newstr2 = newstr.replaceAll("',", '",')
+const newstr3 = newstr2.replaceAll("'.", '".')
+const newstr4 = newstr3.replaceAll("' ", '" ')
+const newstr5 = newstr4.replaceAll("'!", '"!')
 
-  fetchGoods() {
-    this.goods = [
-      { title: "Shirt", price: 150 },
-      { title: "Socks", price: 50 },
-      { title: "Jacket", price: 350 },
-      { title: "Shoes", price: 250 },
-    ];
-  }
+console.log(newstr5)
 
-  render() {
-    let listHtml = "";
-    let totalPrice = 0;
-    this.goods.forEach((good) => {
-      const goodItem = new GoodsItem(good.title, good.price);
-      totalPrice = totalPrice + good.price;
-      listHtml += goodItem.render();
-    });
-    console.log("Суммарная стоисмость всех товаров равна " + totalPrice);
-    document.querySelector(".goods-list").innerHTML = listHtml;
-  }
-}
+// let text = 'AAAADр ролр олр олр олрлор лор р олрл';
+// text1 = text.replace('p', 'я')
 
+// console.log(text1)
 
-const init = () => {
-    const list = new GoodsList();
-    list.fetchGoods();
-    list.render();
-};
+  // const option1 = new RegExp('pattern')
+  // const option2 = /pattern/
 
-window.onload = init;
+  // const firstRegExp = /'/
+
+  // const ourRegExp = /(g(e?|o?))/gi
+  // const str = 'Geeee Go oG Geo'
+
+  // console.log(str.match(ourRegExp))
+  
+
+  // const numberRegExp = /^[a-zA-Z]+$/
+  // const str = 'ASDasdf'
+
+  // console.log(numberRegExp.test(str))
