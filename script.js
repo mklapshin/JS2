dda6a2973d54ac567b8759380b4254c56c545ed9
mklapshin <golub.97@mail.ru>
2021-06-14 14:58:54 +0300

    выполнил домашнее задание 5

--- script.js
+++ script.js
@@ -1,45 +1,74 @@
-class GoodsItem {
-  constructor(title, price) {
-    this.title = title;
-    this.price = price;
+const API_URL =
+  "https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses";
+
+
+console.log(window)
+
+const app = new Vue({
+  el: "#app",
+  data: {
+    goods: [],
+    filteredGoods: [],
+    basketGoods: [],
+    searchLine: '',
+    checkBasket: 0,
+    name: 'ноутбук',
+    showBasket: false,
+    showItem1: false,
+    showItem2: false,
+  },
+
+
+  methods: {
+
+    async getProducts() {
+      const responce = await fetch(`${API_URL}/catalogData.json`);
+      if (responce.ok) {
+        const catalogItems = await responce.json();
+        this.goods = catalogItems;
+        this.filteredGoods = catalogItems;
+      } else {
+        alert("Ошибка при соединении с сервером");
+      }
+    },
+
+    async addToBasket(e) {
+      this.showBasket = true;
+      if (e.target.innerText == '1000') {
+        this.showItem2 = true;
+      } else if (e.target.innerText == 'Мышка') {
+        this.showItem2 = true;
+      }
+      else if (e.target.innerText == 'Ноутбук') {
+        this.showItem1 = true;
+      }
+      else if (e.target.innerText == '45600') {
+        this.showItem1 = true;
+      }
+      },
+
+
+  },
+
+  async mounted() {
+    await this.getProducts()
   }
-  render() {
-    return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p></div>`;
-  }
-}
+});
 
-class GoodsList {
-  constructor() {
-    this.goods = [];
-  }
+// const init = async () => {
+ // const list = new GoodsList();
+  // const busket = new BucketList();
+  // await list.fetchGoods();
+  // list.render();
 
-  fetchGoods() {
-    this.goods = [
-      { title: "Shirt", price: 150 },
-      { title: "Socks", price: 50 },
-      { title: "Jacket", price: 350 },
-      { title: "Shoes", price: 250 },
-    ];
-  }
+//  document.querySelectorAll(".buyButton").forEach((element) => {
+ //   element.addEventListener("click", (event) => {
+ //    const itemId = event.target.getAttribute("itemId");
+  //    const item = list.goods.find(good => good.id_product === parseInt(itemId))
 
-  render() {
-    let listHtml = "";
-    let totalPrice = 0;
-    this.goods.forEach((good) => {
-      const goodItem = new GoodsItem(good.title, good.price);
-      totalPrice = totalPrice + good.price;
-      listHtml += goodItem.render();
-    });
-    console.log("Суммарная стоисмость всех товаров равна " + totalPrice);
-    document.querySelector(".goods-list").innerHTML = listHtml;
-  }
-}
+ //     busket.goods.push(item)
+//      busket.render()
 
-
-const init = () => {
-    const list = new GoodsList();
-    list.fetchGoods();
-    list.render();
-};
-
-window.onload = init;
+//    });
+//  });
+// };
